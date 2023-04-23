ac58eb74ed44e49dfc917c774d3eb9ae30e04abd
MCAI <min.cai.china@gmail.com>
2023-04-23 05:23:09 +0000

    Update scripts.

--- dosbox.sh
+++ dosbox.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# Update package list and install necessary dependencies
-sudo apt-get update
-sudo apt-get install -y dosbox
+# # Update package list and install necessary dependencies
+# sudo apt-get update
+# sudo apt-get install -y dosbox
 
 rm -rf HELLO.EXE HELLO.OBJ
 dosbox compile.bat
