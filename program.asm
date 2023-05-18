1125efa3b9f4229f38fb088efd679f80bbc2436b
Min Cai <min.cai.china@gmail.com>
2023-05-19 01:22:01 +0800

    Update files.

--- program.asm
+++ program.asm
@@ -1,34 +1,56 @@
+; 数据段，存放数据
 data segment
-    welcomeMessage db 'Hello BJUT 80986 MASM assembly programming!', '$' ; 欢迎信息
+    ; 定义欢迎信息字符串，以 $ 结尾
+    welcomeMessage db 'Hello BJUT 80986 MASM assembly programming!', '$'
 data ends
 
+; 堆栈段，存放堆栈数据
 stack segment para stack
+    ; 分配 100h 字节的堆栈空间，初始化为 0
     db 100h dup(0)
 stack ends
 
+; 代码段，存放程序代码
 code segment
+    ; 告知编译器 cs 代表代码段，ds 代表数据段，ss 代表堆栈段
     assume cs: code, ds: data, ss: stack
+
+; 程序入口点
 start:
-    mov ax, data ; 初始化
+    ; 将数据段地址传给 ax
+    mov ax, data
+    ; 初始化数据段寄存器 ds
     mov ds, ax
 
-    lea dx, welcomeMessage ; 提示
+    ; 将欢迎信息字符串地址传给 dx
+    lea dx, welcomeMessage
+    ; 调用 WriteString 过程，将字符串写入标准输出
     call WriteString
 
-    call Terminate ; 终止程序
+    ; 调用 Terminate 过程，终止程序
+    call Terminate
 
 ; 将 dx 中的字符串写入标准输出
 WriteString proc
+    ; 设置 ah 为 09h，对应 DOS 功能 09h：写字符串至标准输出
     mov ah, 09h
+    ; 调用 DOS 中断 21h，执行相应功能
     int 21h
+    ; 返回调用处
     ret
 WriteString endp
 
+; 终止程序
 Terminate PROC
-    mov ax, 4C00h         ; DOS 功能 4Ch: 终止程序
-    int 21h               ; 调用 DOS 中断
+    ; 设置 ax 为 4C00h，对应 DOS 功能 4Ch：终止程序
+    mov ax, 4C00h
+    ; 调用 DOS 中断 21h，执行相应功能
+    int 21h
+    ; 返回调用处
     ret
 Terminate ENDP
 
+; 结束代码段
 code ends
+    ; 设置程序入口点为 start
     end start
